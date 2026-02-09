ff7ae513e9da20cb27d7a84b6cbd54b8de751c28
McCanniel <danielmccann1109@gmail.com>
2026-02-09 22:51:07 +0000

    sdf

--- Assets/driving.cs
+++ Assets/driving.cs
@@ -1,5 +1,6 @@
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,6 +10,16 @@ public class driving : MonoBehaviour
 	float direction;
 	public float accel;
 	public float braking;
+	public float friction;
+
+	enum state { 
+	
+		stationary,
+		forward,
+		barckward
+	}
+
+	state drivingState;
 
 	InputAction accelerate;
 	InputAction brake;
@@ -20,13 +31,14 @@ public class driving : MonoBehaviour
 		accelerate = InputSystem.actions.FindAction("Accelerate");
 		brake = InputSystem.actions.FindAction("Brake");
 		steering = InputSystem.actions.FindAction("Steering");
+
+		direction = transform.rotation.eulerAngles.z;
 	
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
 		if (accelerate.IsPressed() == true)
 		{
 			speed += accel * Time.deltaTime;
@@ -38,13 +50,30 @@ public class driving : MonoBehaviour
 			print("breaking " + brake.IsPressed());
 		}
 
-		speed *= 0.95f;
+        if (speed > 0)
+        {
+            drivingState = state.forward;
+			speed -= friction;
+			if (speed < 0) {speed = 0;}
+        }
+        else if (speed < 0)
+        {
+            drivingState = state.barckward;
+			speed += friction;
+			if (speed > 0) { speed = 0;}
+        }
+        else if (speed == 0)
+        {
+            drivingState = state.stationary;
+        }
 
-		if (math.abs(speed) < 0.0001) { speed = 0; }
+		
+
+        if (math.abs(speed) < 0.0001) { speed = 0; }
 
 		print(speed);
 		//if (steering.IsPressed() == true)
 
-		transform.position.x += math.sin(speed)*Time.deltaTime;
+		transform.position += new Vector3(speed * math.sin(direction)*Time.deltaTime, speed * math.cos(direction)*Time.deltaTime);
 	}
 }
