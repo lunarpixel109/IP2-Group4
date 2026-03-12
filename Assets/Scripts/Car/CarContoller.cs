d6f7a908ae1276a5960b42ff381b737e6e48004a
LunarPixel <ssmith313@caledonian.ac.uk>
2026-03-12 12:19:23 +0000

    Merge branch 'car'

--- Assets/Scripts/Car/CarContoller.cs
+++ Assets/Scripts/Car/CarContoller.cs
@@ -10,10 +10,13 @@ public class CarController : MonoBehaviour
 	public float accel;
 	public float braking;
 	public float friction;
+	public float drift_speed_threshold;
 
 	public float max_speed;
 
 	public float steering_speed;
+	public float drift_steering_speed_max;
+	public float drift_steering_speeed_min;
 
 	InputAction accelerate;
 	InputAction brake;
@@ -34,9 +37,13 @@ public class CarController : MonoBehaviour
 	public float rb_speed_right;
 
 	float rb_direction;
+	int drift_direction;
 
-	DrivingState drivingState = DrivingState.stationary;
-	bool is_drifting = false;
+
+    DrivingState drivingState = DrivingState.stationary;
+
+	public bool is_drifting = false;
+	public float drifting_value = 0f;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
@@ -44,6 +51,7 @@ public class CarController : MonoBehaviour
 		accelerate = InputSystem.actions.FindAction("Accelerate");
 		brake = InputSystem.actions.FindAction("Brake");
 		steering = InputSystem.actions.FindAction("Steering");
+		drifting = InputSystem.actions.FindAction("Drifting");
 
 		rb = GetComponent<Rigidbody2D>();
 	}
@@ -57,85 +65,125 @@ public class CarController : MonoBehaviour
 		rb_speed_forward = rb_speed_local[1]; // splits local space vector into forward component and right component
 		rb_speed_right = rb_speed_local[0];
 
+		if (rb_speed_forward > 0f) { drivingState = DrivingState.forward; }
+		else if (rb_speed_forward < 0f) { drivingState = DrivingState.barckward; }
+		else { drivingState = DrivingState.stationary; }
+
 		rb_direction = rb.rotation; // gets rotation of car via rigidbody2D
 
-		//if (drifting.IsPressed() && rb_speed_forward >= 300) { is_drifting = true; }
+		if (Drift_Check())
+		{
+			if (!is_drifting)
+			{
+				print("drift start");
+				drift_direction = (int)math.sign(steering.ReadValue<float>());
+            }
+			drifting_value += 2f * Time.fixedDeltaTime;
+			if (drifting_value > 1f)
+			{
+				drifting_value = 1f;
+			}
+		}
+		else { drifting_value -= 2f * Time.fixedDeltaTime; if (drifting_value < 0f) { drifting_value = 0f; } }
+
+		if (drifting_value > 0f) {is_drifting = true; print("drifting"); }
+		else {is_drifting = false; }
+
 		#endregion
 
-		#region parallel movement
+		#region parallel movementel
+		//normal
+		if (!is_drifting)
+		{
+			// iffs decide which forces apply to car each frame
+			if (drivingState == DrivingState.forward)
+			{
+				if (accelerate.IsPressed())
+				{
+					rb_speed_forward += (accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime; // increments forward speed by appropriate value based on input and context
+					if (rb_speed_forward > max_speed) { rb_speed_forward = max_speed; }
+					print("accel " + accelerate.IsPressed());
+				}
+				else if (brake.IsPressed())
+				{
+					rb_speed_forward -= braking * Time.fixedDeltaTime;
+					//if (rb_speed_forward < 0) { Momentum_Change_Stationary(); }
+					print("breaking " + brake.IsPressed());
+				}
+				else
+				{
+					rb_speed_forward -= friction * Time.fixedDeltaTime;
+					//if (rb_speed_forward < 0) { Momentum_Change_Stationary(); }
+				}
+			}
+			else if (drivingState == DrivingState.barckward)
+			{
+				if (brake.IsPressed())
+				{
+					rb_speed_forward -= (accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
+					print("reversing " + brake.IsPressed());
+				}
+				else if (accelerate.IsPressed())
+				{
+					rb_speed_forward += braking * Time.fixedDeltaTime;
+					//if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
+					print("R breaking " + accelerate.IsPressed());
+				}
+				else
+				{
+					rb_speed_forward += friction * Time.fixedDeltaTime;
+					//if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
+				}
+			}
+			else if (drivingState == DrivingState.stationary)
+			{
+				if (accelerate.IsPressed())
+				{
+					rb_speed_forward += (accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime;
+					print("reversing " + brake.IsPressed());
+				}
+				else if (brake.IsPressed())
+				{
+					rb_speed_forward -= (accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
+					//if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
+					print("R breaking " + accelerate.IsPressed());
+				}
+			}
+		}
+		// drifting
+		else
+		{
+            // rb_speed_forward starts constant
+        }
+        #endregion
 
-		// iffs decide which forces apply to car each frame
-		if (drivingState == DrivingState.stationary) // stationary == no forces
-		{
-			if (accelerate.IsPressed()) { drivingState = DrivingState.forward; }
-			else if (brake.IsPressed()) { drivingState = DrivingState.barckward; }
-		}
-		else if (drivingState == DrivingState.forward)
-		{
-			if (accelerate.IsPressed())
-			{
-				rb_speed_forward += (accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime; // increments forward speed by appropriate value based on input and context
-                rb_speed_forward = Mathf.Clamp(rb_speed_forward, -max_speed, max_speed);
-                if (rb_speed_forward > max_speed) { rb_speed_forward = max_speed; }
-				print("accel " + accelerate.IsPressed());
-			}
-			else if (brake.IsPressed())
-			{
-				rb_speed_forward -= braking * Time.fixedDeltaTime;
-                rb_speed_forward = Mathf.Clamp(rb_speed_forward, -max_speed, max_speed);
-                if (rb_speed_forward < 0) { Momentum_Change_Stationary(); }
-				print("breaking " + brake.IsPressed());
-			}
-			else
-			{
-				rb_speed_forward -= friction * Time.fixedDeltaTime;
-                rb_speed_forward = Mathf.Clamp(rb_speed_forward, -max_speed, max_speed);
-                if (rb_speed_forward < 0) { Momentum_Change_Stationary(); }
-			}
-		}
-		else if (drivingState == DrivingState.barckward)
-		{
-			if (brake.IsPressed())
-			{
-				rb_speed_forward -= (accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
-                rb_speed_forward = Mathf.Clamp(rb_speed_forward, -max_speed, max_speed);
-                print("reversing " + brake.IsPressed());
-			}
-			else if (accelerate.IsPressed())
-			{
-				rb_speed_forward += braking * Time.fixedDeltaTime;
-                rb_speed_forward = Mathf.Clamp(rb_speed_forward, -max_speed, max_speed);
-                if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
-				print("R breaking " + accelerate.IsPressed());
-			}
-			else
-			{
-				rb_speed_forward += friction * Time.fixedDeltaTime;
-                rb_speed_forward = Mathf.Clamp(rb_speed_forward, -max_speed, max_speed);
-                if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
-			}
-		}
-		#endregion
-
-		#region perpendicular movement
-		rb_speed_right *= 0.5f; // always lowers right/left speed of car (will not effect drift dont worry)
+        #region perpendicular movement
+        rb_speed_right *= 0.5f; // always lowers right/left speed of car (will not effect drift dont worry)
 		if (math.abs(rb_speed_right) < 0.05) rb_speed_right = 0;
 		#endregion
 
 		#region steering
 		rb.angularVelocity = 0f;
-		if (steering.IsPressed())
+		if (!is_drifting)
 		{
-			if (drivingState == DrivingState.forward)
+			if (steering.IsPressed())
 			{
-				rb_direction -= steering.ReadValue<float>() * Steering_Speed_Curve() * Time.fixedDeltaTime;
-			}
-			else if (drivingState == DrivingState.barckward)
-			{
-				rb_direction += steering.ReadValue<float>() * Steering_Speed_Curve() * Time.fixedDeltaTime;
-			}
+				if (drivingState == DrivingState.forward)
+				{
+					rb_direction -= steering.ReadValue<float>() * Steering_Speed_Curve() * Time.fixedDeltaTime;
+				}
+				else if (drivingState == DrivingState.barckward)
+				{
+					rb_direction += steering.ReadValue<float>() * Steering_Speed_Curve() * Time.fixedDeltaTime;
+				}
 
-			print("steering" + steering.ReadValue<float>());
+				print("steering" + steering.ReadValue<float>());
+			}
+		}
+		else
+		{
+			rb_direction -= (Mathf.Lerp(drift_steering_speeed_min, drift_steering_speed_max, Mathf.InverseLerp(1, -1, steering.ReadValue<float>())) * drift_direction) * Time.fixedDeltaTime;
+			print("drifting");
 		}
 		#endregion
 
@@ -148,7 +196,6 @@ public class CarController : MonoBehaviour
 		#endregion
 	}
 
-
 	void Momentum_Change_Stationary()
 	{
 		rb_speed_forward = 0;
@@ -160,35 +207,17 @@ public class CarController : MonoBehaviour
 		return steering_speed;
 	}
 
-    public void ApplySpeedMultiplier(float multiplier, float duration)
-    {
-        StartCoroutine(SpeedMultiplierRoutine(multiplier, duration));
-    }
-    
-    IEnumerator SpeedMultiplierRoutine(float multiplier, float duration)
-    {
-        float originalAccel = accel;
-        float originalFriction = friction;
-        float originalMaxSpeed = max_speed;
-
-        accel *= multiplier;
-        friction *= multiplier;
-        max_speed *= multiplier;
-
-        yield return new WaitForSeconds(duration);
-
-        accel = originalAccel;
-        friction = originalFriction;
-        max_speed = originalMaxSpeed;
-    }
-
-    public void ApplyKnockBack(Vector2 worldForce, float forwardLoss)
-    {
-        rb.linearVelocity += worldForce;
-
-        rb_speed_local = rb.GetVector(rb.linearVelocity);
-        rb_speed_forward *= forwardLoss;
-        rb_speed_right = rb_speed_local.x;
-    }
-
+	bool Drift_Check()
+	{
+		if (
+			drifting.IsPressed() && drivingState == DrivingState.forward
+			&& !brake.IsPressed() && accelerate.IsPressed() &&
+			((!is_drifting && steering.IsInProgress()) || is_drifting)
+			&& rb_speed_forward >= drift_speed_threshold
+			)
+		{
+			return true;
+		}
+		else { return false; }
+	}
 }
