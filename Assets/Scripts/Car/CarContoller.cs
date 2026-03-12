a721caefeebce8dd595996b48a80263ba24b1e00
McCanniel <danielmccann1109@gmail.com>
2026-03-12 22:38:37 +0000

    drift + boost

--- Assets/Scripts/Car/CarContoller.cs
+++ Assets/Scripts/Car/CarContoller.cs
@@ -1,57 +1,84 @@
 using System;
+using System.Collections;
+using System.Numerics;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
-	public float accel;
-	public float braking;
-	public float friction;
-	public float drift_speed_threshold;
+    [Header("Other Info")]
 
-	public float max_speed;
+    Rigidbody2D rb;
+    public GameObject carSprite;
 
-	public float steering_speed;
-	public float drift_steering_speed_max;
-	public float drift_steering_speeed_min;
+    InputAction accelerate;
+    InputAction brake;
+    InputAction steering;
+    InputAction drifting;
+	InputAction boosting;
 
-	InputAction accelerate;
-	InputAction brake;
-	InputAction steering;
-	InputAction drifting;
-
-	enum DrivingState
-	{
-		stationary,
-		forward,
-		barckward
-	}
-
-	Rigidbody2D rb;
-
-	public Vector2 rb_speed_local;
-	public float rb_speed_forward;
-	public float rb_speed_right;
-
-	float rb_direction;
-	int drift_direction;
-
+    enum DrivingState
+    {
+        stationary,
+        forward,
+        barckward
+    }
 
     DrivingState drivingState = DrivingState.stationary;
 
-	public bool is_drifting = false;
-	public float drifting_value = 0f;
+    [Header("Driving Setup")]
 
-	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	public float accel;
+	public float braking;
+	public float friction;
+    public float steering_speed;
+    public float max_speed;
+	public float max_speed_reverse;
+	public float boost_accel;
+	public float boost_max_speed;
+	public float boost_slowdown;
+
+    float _accel;
+	float _max_speed;
+
+    [Header("Drifting Setup")]
+
+    public float drift_speed_threshold;
+	public float drift_steering_speed_max;
+	public float drift_steering_speeed_min;
+	public float drift_transition_time_seconds;
+	float drift_transition_time;
+
+	public float drift_boost_gain;
+
+	[Header("Live Info")]
+
+	public UnityEngine.Vector2 rb_speed_local;
+	public float rb_speed_forward;
+	public float rb_speed_right;
+
+	public float rb_direction;
+	public float carSprite_direction;
+	int drift_direction;
+
+	public float boost;
+
+    public bool is_drifting = false;
+    public float drifting_value = 0f;
+
 	void Start()
 	{
 		accelerate = InputSystem.actions.FindAction("Accelerate");
 		brake = InputSystem.actions.FindAction("Brake");
 		steering = InputSystem.actions.FindAction("Steering");
 		drifting = InputSystem.actions.FindAction("Drifting");
+		boosting = InputSystem.actions.FindAction("Boost");
 
 		rb = GetComponent<Rigidbody2D>();
+		_accel = accel;
+		_max_speed = max_speed;
+		drift_transition_time = 1/drift_transition_time_seconds;
 	}
 
 	// Update is called once per frame
@@ -69,22 +96,49 @@ public class CarController : MonoBehaviour
 
 		rb_direction = rb.rotation; // gets rotation of car via rigidbody2D
 
+		if (boosting.IsPressed()) { boost += 0.5f * Time.fixedDeltaTime; }
+		else
+		{
+			if (!is_drifting)
+			{
+				boost -= 1f * Time.fixedDeltaTime;
+				if (boost < 0f) { boost = 0f; Boost_Cancel(); }
+			}
+        }
+
+		if (boost > 3f) { boost = 3f; }
+
+		if (boost > 0f && !is_drifting)
+		{
+			_max_speed = boost_max_speed;
+			_accel = boost_accel;
+		}
+
 		if (Drift_Check())
 		{
 			if (!is_drifting)
 			{
 				print("drift start");
 				drift_direction = (int)math.sign(steering.ReadValue<float>());
-            }
-			drifting_value += 2f * Time.fixedDeltaTime;
+				rb_speed_forward -= 2f;
+			}
+			drifting_value += drift_transition_time * Time.fixedDeltaTime;
 			if (drifting_value > 1f)
 			{
 				drifting_value = 1f;
 			}
 		}
-		else { drifting_value -= 2f * Time.fixedDeltaTime; if (drifting_value < 0f) { drifting_value = 0f; } }
+		else
+		{
+			drifting_value -= drift_transition_time * Time.fixedDeltaTime;
+			if (drifting_value < 0f) { drifting_value = 0f; }
+		}
 
-		if (drifting_value > 0f) {is_drifting = true; print("drifting"); }
+		if (drifting_value > 0f)
+		{
+			is_drifting = true; print("drifting");
+			boost += drift_boost_gain * Time.fixedDeltaTime;
+		}
 		else {is_drifting = false; }
 
 		#endregion
@@ -98,59 +152,64 @@ public class CarController : MonoBehaviour
 			{
 				if (accelerate.IsPressed())
 				{
-					rb_speed_forward += (accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime; // increments forward speed by appropriate value based on input and context
-					if (rb_speed_forward > max_speed) { rb_speed_forward = max_speed; }
+					rb_speed_forward += (_accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime; // increments forward speed by appropriate value based on input and context
+					if (rb_speed_forward > _max_speed) { var time = Time.fixedDeltaTime; Max_speed_clamp(time); }
 					print("accel " + accelerate.IsPressed());
 				}
 				else if (brake.IsPressed())
 				{
 					rb_speed_forward -= braking * Time.fixedDeltaTime;
-					//if (rb_speed_forward < 0) { Momentum_Change_Stationary(); }
+					if (rb_speed_forward < 0) { rb_speed_forward = 0; }
+					Boost_Cancel();
 					print("breaking " + brake.IsPressed());
 				}
 				else
 				{
 					rb_speed_forward -= friction * Time.fixedDeltaTime;
-					//if (rb_speed_forward < 0) { Momentum_Change_Stationary(); }
+					if (rb_speed_forward < 0) { rb_speed_forward = 0; }
+					Boost_Cancel();
 				}
 			}
 			else if (drivingState == DrivingState.barckward)
 			{
 				if (brake.IsPressed())
 				{
-					rb_speed_forward -= (accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
-					print("reversing " + brake.IsPressed());
+					rb_speed_forward -= (_accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
+                    if (rb_speed_forward < max_speed_reverse) { rb_speed_forward = -max_speed_reverse; }
+                    print("reversing " + brake.IsPressed());
 				}
 				else if (accelerate.IsPressed())
 				{
 					rb_speed_forward += braking * Time.fixedDeltaTime;
-					//if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
+					if (rb_speed_forward > 0) { rb_speed_forward = 0; }
 					print("R breaking " + accelerate.IsPressed());
 				}
 				else
 				{
 					rb_speed_forward += friction * Time.fixedDeltaTime;
-					//if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
+					if (rb_speed_forward > 0) { rb_speed_forward = 0; }
 				}
 			}
 			else if (drivingState == DrivingState.stationary)
 			{
 				if (accelerate.IsPressed())
 				{
-					rb_speed_forward += (accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime;
-					print("reversing " + brake.IsPressed());
-				}
+                    rb_speed_forward += (_accel - (rb_speed_forward / 10)) * Time.fixedDeltaTime;
+                    if (rb_speed_forward > _max_speed) { rb_speed_forward = _max_speed; }
+                    print("accel " + accelerate.IsPressed());
+                }
 				else if (brake.IsPressed())
 				{
-					rb_speed_forward -= (accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
-					//if (rb_speed_forward > 0) { Momentum_Change_Stationary(); }
-					print("R breaking " + accelerate.IsPressed());
-				}
+                    rb_speed_forward -= (_accel / 2 + rb_speed_forward / 10) * Time.fixedDeltaTime;
+                    if (rb_speed_forward < max_speed_reverse) { rb_speed_forward = -max_speed_reverse; }
+                    print("reversing " + brake.IsPressed());
+                }
 			}
 		}
 		// drifting
 		else
 		{
+            if (rb_speed_forward > _max_speed) { rb_speed_forward = _max_speed; }
             // rb_speed_forward starts constant
         }
         #endregion
@@ -161,10 +220,15 @@ public class CarController : MonoBehaviour
 		#endregion
 
 		#region steering
+
 		rb.angularVelocity = 0f;
+		carSprite_direction = carSprite.transform.eulerAngles.z;
+
 		if (!is_drifting)
 		{
-			if (steering.IsPressed())
+            //carSprite.transform.rotation = quaternion.EulerXYZ(0, 0, transform.eulerAngles.z);
+
+            if (steering.IsPressed())
 			{
 				if (drivingState == DrivingState.forward)
 				{
@@ -180,24 +244,21 @@ public class CarController : MonoBehaviour
 		}
 		else
 		{
-			rb_direction -= (Mathf.Lerp(drift_steering_speeed_min, drift_steering_speed_max, Mathf.InverseLerp(1, -1, steering.ReadValue<float>())) * drift_direction) * Time.fixedDeltaTime;
+			var drift_amount = Mathf.Lerp(drift_steering_speeed_min, drift_steering_speed_max, Mathf.InverseLerp(1, -1, steering.ReadValue<float>())) * drift_direction * drifting_value;
+
+            rb_direction = drift_amount * Time.fixedDeltaTime;
+			//carSprite.transform.rotation = quaternion.EulerXYZ(0, 0, transform.eulerAngles.z + drift_amount);
 			print("drifting");
 		}
 		#endregion
 
 		#region output
 
-		rb_speed_local = new Vector2(rb_speed_right, rb_speed_forward); // recombines forward and right vectors
+		rb_speed_local = new UnityEngine.Vector2(rb_speed_right, rb_speed_forward); // recombines forward and right vectors
 		rb.linearVelocity = rb.GetRelativeVector(rb_speed_local); // sets speed in global space based on speed in local space - NOT WORKING IDK WHY
 
 		rb.rotation = rb_direction;
 		#endregion
-	}
-
-	void Momentum_Change_Stationary()
-	{
-		rb_speed_forward = 0;
-		drivingState = DrivingState.stationary;
 	}
 
 	float Steering_Speed_Curve()
@@ -208,14 +269,33 @@ public class CarController : MonoBehaviour
 	bool Drift_Check()
 	{
 		if (
-			drifting.IsPressed() && drivingState == DrivingState.forward
-			&& !brake.IsPressed() && accelerate.IsPressed() &&
-			((!is_drifting && steering.IsInProgress()) || is_drifting)
+			drifting.IsPressed()
+			&& drivingState == DrivingState.forward
+			&& !brake.IsPressed() && accelerate.IsPressed()
+			&& ((!is_drifting && steering.IsInProgress()) || is_drifting)
 			&& rb_speed_forward >= drift_speed_threshold
+			&& boost > 0f
 			)
 		{
 			return true;
 		}
 		else { return false; }
 	}
+	void Max_speed_clamp(float deltaTime)
+	{
+		if (_max_speed - rb_speed_forward < -0.5f)
+		{
+			rb_speed_forward -= boost_slowdown * deltaTime;
+		}
+		else
+		{
+			rb_speed_forward = _max_speed;
+		}
+	}
+
+	void Boost_Cancel()
+	{
+        _max_speed = max_speed;
+        _accel = accel;
+    }
 }
