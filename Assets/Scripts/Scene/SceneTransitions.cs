7654ec12ddcc0e120bf2afbd8224a3c9b78873a9
Bobbins909 <robyn.cairns4@gmail.com>
2026-03-25 21:46:45 +0000

    Hide screen UI

--- Assets/Scripts/Scene/SceneTransitions.cs
+++ Assets/Scripts/Scene/SceneTransitions.cs
@@ -4,30 +4,40 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransitions : MonoBehaviour
 {
+    // when setting up references just drag the canvases of the track map, timer and player icon in 
+
     public Animator TransitionAnim1;
     public Animator TransitionAnim2;
 
     public GameObject TrackMap;
+    public GameObject PlayerIcon;
+    public GameObject Timer;
 
     public string SceneName;
-    void Update()
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            StartCoroutine(LoadScene());
+        TrackMap.SetActive(false);
+        PlayerIcon.SetActive(false);
+        Timer.SetActive(false);
 
-            TrackMap.SetActive(false);
-        }
+        StartCoroutine(HideUI());
     }
-
     public IEnumerator LoadScene()
     {
         TransitionAnim1.SetTrigger("End");
         TransitionAnim2.SetTrigger("End");
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(SceneName);
-        yield return new WaitForSeconds(1.5f);
+    }
+    public IEnumerator HideUI()
+    {
+        yield return new WaitForSeconds(1f);
+    
         TrackMap.SetActive(true);
+        PlayerIcon.SetActive(true);
+        Timer.SetActive(true);
+
     }
 
 }
