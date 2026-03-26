70326880f6e4fb382cd2e5798efaeae0261b05e4
Rocco156 <mrkingdice74@gmail.com>
2026-03-26 19:22:03 +0000

    fixed issue with audio manager dummy

--- Assets/Scripts/Managers/AudioManagerDummyScript.cs
+++ Assets/Scripts/Managers/AudioManagerDummyScript.cs
@@ -2,25 +2,12 @@ using UnityEngine;
 
 public class AudioManagerDummyScript : MonoBehaviour
 {
-    
+
 
 
     // used to make sure audio files where being played correctly
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            AudioManager.PlaySound(Sound_types., 0);
-        } else if (Input.GetKeyDown(KeyCode.S))
-        {
-            AudioManager.PlaySound(Sound_types., 0);
-        } else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            AudioManager.PlaySound(Sound_types., 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            AudioManager.PlayRandomSound(Sound_types.);
-        }
+        return;
     }
 }
