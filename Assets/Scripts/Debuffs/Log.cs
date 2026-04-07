5f9f9e31ff3f340b04a4987329c240d34d0237b6
Bobbins909 <robyn.cairns4@gmail.com>
2026-04-07 19:53:25 +0100

    Log added

--- Assets/Scripts/Debuffs/Log.cs
+++ Assets/Scripts/Debuffs/Log.cs
@@ -1,14 +1,57 @@
 using UnityEngine;
+using System.Collections;
 
-public class Log : MonoBehaviour
+public class LogCollisionHandler : MonoBehaviour
 {
-    void Start()
+    public float knockbackForce = 10f;
+    [Range(0, 1)] public float forwardSpeedLoss = 0.2f;
+    public float fadeDuration = 1f; 
+
+    private Animator logAnimator;
+    private SpriteRenderer spriteRenderer;
+    private bool isDestroying = false;
+
+    void Awake() // Runs before Start()
     {
-        
+        logAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        logAnimator.SetFloat("AnimSpeed", 0f);
     }
 
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player") && !isDestroying)
+        {
+            CarController car = collision.gameObject.GetComponent<CarController>();
+            if (car != null)
+            {
+                Vector2 bounceDirection = (collision.transform.position - transform.position).normalized;
+                car.ApplyKnockBack(bounceDirection * knockbackForce, forwardSpeedLoss);
+
+                logAnimator.SetFloat("AnimSpeed", 1f);
+
+                StartCoroutine(WaitAndFade());
+            }
+        }
+    }
+
+    IEnumerator WaitAndFade()
+    {
+        isDestroying = true;
+
+        yield return new WaitUntil(() => logAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f);
+
+        float counter = 0;
+        Color startColor = spriteRenderer.color;
+
+        while (counter < fadeDuration)
+        {
+            counter += Time.deltaTime;
+            float alpha = Mathf.Lerp(1, 0, counter / fadeDuration);
+            spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
